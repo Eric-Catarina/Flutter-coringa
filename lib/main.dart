33befc6d3735e48b3527543da00ab00c9b34d871
Eric-Catarina <2020954200@teiacoltec.org>
2022-09-03 13:58:37 -0300

    Exemplo de Widgets simples

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(Aplicativo)
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      child: Center(
+        child: MeuTexto(),
+      ),
+    );
+  }
+}
+
+class MeuTexto extends StatelessWidget{
+  @override
+  Widget build(BuildContext){
+    return Container(
+      child: Text(
+        "Texto da Classe",
+        textDirection: TextDirection.rtl,
+      ),
+    );
+  }
 }
