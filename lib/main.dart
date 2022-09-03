6a24f9b9c0c13e2323e6146988c23e794d48f0f3
Eric-Catarina <2020954200@teiacoltec.org>
2022-09-03 14:58:13 -0300

    Criação do primeiro botão com ícone

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
 
-void Main(){
+void main(){
   runApp(AppCoringa());
 }
 
 class AppCoringa extends StatelessWidget{
+  const AppCoringa({Key? key}) : super(key: key);
+
   @override
   Widget build (BuildContext context){
-    return Container(
-      child: Center(
-        child: Text("Meu texto",
-        textDirection: TextDirection.rtl,
+    return const MaterialApp(
+      home: HomePageWidget(),
+      );
+  }
+}
+
+class HomePageWidget extends StatelessWidget{
+  const HomePageWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("banaana"),
+      ),
+      body: Center(
+        child:
+        Text("Amongus",
+        textDirection: TextDirection.rtl
+          ),
+      ),
+      floatingActionButton: FloatingActionButton (
+        child: Icon(
+          Icons.favorite_outline_sharp,
+          color: Colors.red,
         ),
+        onPressed: () {print("cliclou");},
       ),
     );
   }
