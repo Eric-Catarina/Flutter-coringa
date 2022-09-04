2ce3308a84d4f8d5416423ead2dcdfcc9b2b8dfc
Eric-Catarina <2020954200@teiacoltec.org>
2022-09-04 20:55:31 -0300

    Alterações Neumorfismo

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -44,8 +44,8 @@ class QuadradoNeumorfico extends StatelessWidget{
      height: 200,
      decoration: BoxDecoration(
        gradient: LinearGradient(
-           begin: Alignment(-1.0, -4.0),
-           end: Alignment(1.0, 4.0),
+           begin: Alignment(-2.0, -8.0),
+           end: Alignment(2.0, 8.0),
            colors: [
              Color(0xFF5bc6ff),
              Color(0xFF4da7db),
@@ -55,14 +55,14 @@ class QuadradoNeumorfico extends StatelessWidget{
        boxShadow: [
          BoxShadow(
              color: Color(0xFF4ca5d8),
-             offset: Offset(5.0, 5.0),
-             blurRadius: 15.0,
-             spreadRadius: 1.0),
+             offset: Offset(30.0, 30.0),
+             blurRadius:30.0,
+             spreadRadius: 2.0),
          BoxShadow(
              color: Color(0xFF5ecdff),
-             offset: Offset(-5.0, -5.0),
-             blurRadius: 15.0,
-             spreadRadius: 1.0),
+             offset: Offset(-30.0, -30.0),
+             blurRadius: 30.0,
+             spreadRadius: 2.0),
        ]
      ),
    );
