d274b28f52a6b5e9f95df417dd857e52f8425f1b
Eric-Catarina <2020954200@teiacoltec.org>
2022-09-04 11:10:19 -0300

    Scroll

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -23,34 +23,62 @@ class _HomePageState extends State<HomePage>{
         title: Text("Fazendo o Curso :D"),
       ),
       body: Center(
-        child: Column(children: [
-          Quadrado(Colors.amberAccent),
-          Quadrado(Colors.blueGrey),
-        ],
-        ),
+
+          child: SingleChildScrollView(
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+            PilhaQuadrados(),
+              PilhaQuadrados(),
+              PilhaQuadrados(),
+              PilhaQuadrados(),
+              PilhaQuadrados(),
+              PilhaQuadrados(),
+        ],),
+          )
       ),
-
     );
-
   }
 
 }
 
 class Quadrado extends StatelessWidget{
-  
+
   Color cor;
 
   Quadrado(this.cor);
 
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 80,
-      height: 80,
+      width: 65,
+      height: 65,
       color: cor,
     );
   }
 
 }
 
+class PilhaQuadrados extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Quadrado(Colors.cyan),
+      Quadrado(Colors.amber),
+      Quadrado(Colors.purple),
+    ]);
+  }
+}
+
+class FilaQuadrados extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Quadrado(Colors.cyan),
+      Quadrado(Colors.amber),
+      Quadrado(Colors.purple),
+    ]);
+  }
+}
